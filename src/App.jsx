fc0c270f328a406c4e2d4105cd03bafd38688340
PabloRafael-coder <pabloofael@gmail.com>
2024-07-17 22:25:01 -0300

    modified header component in app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import './global.css'
+import { Header } from "./components/Header"
 
 function App() {
-
   return (
-    <h1>Hello world</h1>
+    <>
+      <Header />
+      <main>
+        <section>
+
+        </section>
+      </main>
+    </>
   )
 }
 
