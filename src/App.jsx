549041dd55080e7549daa0e8a22bcd85d44e5bd4
PabloRafael-coder <pabloofael@gmail.com>
2024-07-23 15:58:09 -0300

    feat: add main structure and content to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import { Header } from "./components/Header";
-function App() {
+import './global.css'
+import styles from './App.module.css'
+import { Header } from "./components/Header"
 
+function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <section className={styles.sectionContent}>
+          <div className={styles.textContent}>
+            <h1>Olá, sou Pablo Rafael</h1>
+            <p>
+              Com uma sólida formação em JavaScript e especialização em React,
+              trago uma abordagem abrangente e inovadora ao desenvolvimento web,
+              atuando com maestria tanto no front-end quanto no back-end. Minha
+              habilidade em criar interfaces dinâmicas e intuitivas se alia à
+              competência em gerenciar servidores e bancos de dados, garantindo
+              uma experiência completa e otimizada para o usuário.
+            </p>
+          </div>
+
+        </section>
+      </main>
+    </>
   )
 }
 
