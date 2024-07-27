d75b82106c6cd3b8acc899b17376cf66e45ce3de
PabloRafael-coder <pabloofael@gmail.com>
2024-07-26 21:45:13 -0300

    feat: Create cardSkill component and style

---
+++ src/components/CardSkill.jsx
@@ -0,0 +1,14 @@
+import styles from './CardSkill.module.css'
+
+export function CardSkill() {
+    return (
+        <div className={styles.content}>
+            <h3>Web Developer</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Itaque minus adipisci sequi iure laborum, perferendis quaerat
+                voluptatibus, nesciunt nisi dolor laboriosam porro nostrum enim
+                corrupti recusandae impedit architecto fuga sint!
+            </p>
+        </div>
+    )
+}
