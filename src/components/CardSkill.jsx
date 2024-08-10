6be5c802fff9c624f2ef9d8f18bef9f85cb6c19e
PabloRafael-coder <pabloofael@gmail.com>
2024-08-09 21:24:39 -0300

    feat: Add props to component.

--- src/components/CardSkill.jsx
+++ src/components/CardSkill.jsx
@@ -1,14 +1,9 @@
 import styles from './CardSkill.module.css'
 
-export function CardSkill() {
+export function CardSkill({ children }) {
     return (
         <div className={styles.content}>
-            <h3>Web Developer</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Itaque minus adipisci sequi iure laborum, perferendis quaerat
-                voluptatibus, nesciunt nisi dolor laboriosam porro nostrum enim
-                corrupti recusandae impedit architecto fuga sint!
-            </p>
+            {children}
         </div>
     )
 }
