79289c1c5c866946b5fe83c079f476b0c50d4fca
PabloRafael-coder <pabloofael@gmail.com>
2024-08-15 21:13:17 -0300

    feat: Add new CardSkills component

---
+++ src/components/CardSkills.jsx
@@ -0,0 +1,9 @@
+import styles from './CardSkills.module.css'
+
+export function CardSkill({ children }) {
+    return (
+        <div className={styles.content}>
+            {children}
+        </div>
+    )
+}
