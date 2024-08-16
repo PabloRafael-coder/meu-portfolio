d893b3173d2b661bc68c995bb87fdba973b2dc6e
PabloRafael-coder <pabloofael@gmail.com>
2024-08-15 21:12:21 -0300

    remove: Remove CardSkill component

--- src/components/CardSkill.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './CardSkill.module.css'
-
-export function CardSkill({ children }) {
-    return (
-        <div className={styles.content}>
-            {children}
-        </div>
-    )
-}
